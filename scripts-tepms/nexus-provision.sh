da2a333d3f51d4fdfc9d7aa993c2a6a9eb131a97
rns <rns@gmail.com>
2018-07-13 11:34:59 +0530

    Nexus Provision script is updated with portainer

--- scripts-tepms/nexus-provision.sh
+++ scripts-tepms/nexus-provision.sh
@@ -35,7 +35,9 @@ dockerinstall () {
 }
 
 startNexusContainer(){
-    sudo docker run -d -m 1536M -p 8081:8081 --name nexus sonatype/nexus3
+    sudo docker run -d -m 1536M -p 8081:8081 --name nexus sonatype/nexus
+	sudo docker volume create portainer_data
+	sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 }
  
 if [ "$relver" = "rhel6" -o "$relver" = "rhel7" ]
