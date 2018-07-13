ff5d98e392d76d8e6d3031ab332b5365b84e6581
rns <rns@gmail.com>
2018-07-13 11:58:36 +0530

    Sudo permission is added to jenkins service

--- scripts-tepms/jmaster-provision.sh
+++ scripts-tepms/jmaster-provision.sh
@@ -4,7 +4,7 @@ installJenkins() {
 	wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 	sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 	apt-get update -y && apt-get install jenkins=2.121.1 -y	# 2.73.3 -y 
-	systemctl start jenkins
+	sudo systemctl start jenkins
 }
 
 installJenkins
